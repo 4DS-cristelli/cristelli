8f49bbc2eac68b6c9825b9fb9da766df4221ead6
eric.cristelli <eric.cristelli@buonarroti.tn.it>
2023-10-10 09:15:07 +0200

    feat: sistemato metodo controllo stringa

--- src/cristelliStudente/Studente.java
+++ src/cristelliStudente/Studente.java
@@ -18,8 +18,8 @@ public class Studente {
             throw new Exception(e.getMessage());
         }
         if(controllaStringa(n) && controllaStringa(c)){
-            this.nome = sistemaStringa(n);
-            this.cognome = sistemaStringa(c);
+            this.nome = n;
+            this.cognome = c;
         }else{
             throw new Exception("Stringa non inizializzata");
         }
@@ -28,9 +28,11 @@ public class Studente {
     }
 
     public Studente(Studente s){
-        this.nome = s.getNome();
-        this.cognome = s.getCognome();
+        this.nome = s.nome;
+        this.cognome = s.cognome;
     }
+
+
 
     public String getNome() {
         return nome;
@@ -47,7 +49,9 @@ public class Studente {
             }
         }catch (ArithmeticException e){
             throw new ArithmeticException(e.getMessage());
-        } catch (Exception e){
+        }catch (NullPointerException e) {
+            throw new NullPointerException("Nome nullo");
+        }catch (Exception e){
             throw new Exception(e.getMessage());
         }
     }
@@ -57,6 +61,8 @@ public class Studente {
             if(controllaStringa(cognome)){
                 this.cognome = cognome;
             }
+        }catch (NullPointerException e) {
+            throw new NullPointerException("Cognome nullo");
         }catch (Exception e){
             throw new Exception(e.getMessage());
         }
@@ -78,31 +84,28 @@ public class Studente {
             throw new ArithmeticException();
         }*/
 
-
-        if(stringa == null){
-            throw new NullPointerException("Stringa a null");
-        }
-
-        if(stringa.length()<3){
-            throw new Exception("Stringa troppo corta");
-        }
-        //controllo i nomi composti es Maria Beatrice
-        String[] tmp = stringa.split(" ");
-        int i;
-        for(i= 0; i<tmp.length;i++){
-            if(!tmp[i].matches("[A-Z][a-z]*")){
-                throw new Exception("iniziali sbagliate");
+        try {
+            if(stringa.length()<3){
+                throw new Exception("Stringa troppo corta");
             }
+            //controllo i nomi composti es Maria Beatrice
+            String[] tmp = stringa.split(" ");
+            if(tmp.length==0){
+                throw new Exception("Stringa vuota");
+            }
+            int i;
+            for(i= 0; i<tmp.length;i++){
+                if(!tmp[i].matches("[A-Z a-z]*")){//
+                    throw new Exception("iniziali sbagliate");
+                }
+            }
+        }catch (NullPointerException e){
+            throw new NullPointerException(e.getMessage());
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
         }
         return true;
     }
 
-    private String sistemaStringa(String s){
-        s = s.toLowerCase(); //rende tutta la stringa minuscola
-        s= s.replaceAll(" ",""); //elima tutti li spazi
-        char tmpC= Character.toUpperCase(s.charAt(0)); //prende la prima lettera e crea un char maiuscolo di quella lettera
-        s = s.substring(1);//tolgo la prima lettera miniscola
-        String def = tmpC+s;//aggiungo la prima lettera precedentemente resa maiuscola
-        return def;
-    }
+
 }
--- src/cristelliStudente/mainTest.java
+++ src/cristelliStudente/mainTest.java
@@ -3,11 +3,12 @@ public class mainTest {
     public static void main(String[] args) {
         Studente s1 = null;
         try{
-            s1 = new Studente("Maria beatrice", "cristelli");
+            s1 = new Studente(null, "Prova");
         }catch (ArithmeticException e){
             System.out.println("divisione per 0");
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
+
     }
 }
