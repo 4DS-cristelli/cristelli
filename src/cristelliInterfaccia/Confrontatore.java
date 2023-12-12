60d645d9acd7bf993b2771d73a0c140b185d9f0d
eric.cristelli <eric.cristelli@buonarroti.tn.it>
2023-12-12 10:20:40 +0100

    feat: idk pt2

---
+++ src/cristelliInterfaccia/Confrontatore.java
@@ -0,0 +1,7 @@
+package cristelliInterfaccia;
+
+public interface Confrontatore {
+    Boolean confrontaUguale(Confrontatore confrontabile);
+    Boolean confrontaMinore(Confrontatore confrontabile);
+    Boolean confrontaMaggiore(Confrontatore confrontabile);
+}
